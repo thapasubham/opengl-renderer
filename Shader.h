2b2b6653ddc1c636ce82248dd10d93a33cbf740b
Subham Thapa <subhamits4.2@gmail.com>
2026-03-30 21:39:35 +0545

    seperated VBO, VAO and EBO in their own class

--- Shader.h
+++ Shader.h
@@ -12,16 +12,12 @@ class Shader
 public:
     unsigned int ID;
 
-    // constructor generates the shader on the fly
-    // ------------------------------------------------------------------------
+   
     Shader(const char* vertexPath, const char* fragmentPath);
 
-    // activate the shader
-    // ------------------------------------------------------------------------
     void use();
 
-    // utility uniform functions
-    // ------------------------------------------------------------------------
+ 
     void setBool(const std::string &name, bool value) const;
     void setInt(const std::string &name, int value) const;
     void setFloat(const std::string &name, float value) const;
--- main.cpp
+++ main.cpp
@@ -3,21 +3,24 @@
 #include <iostream>
 #include "Shader.h"
 #include "stb_image.h"
-void frame_resize_viewport(GLFWwindow*, int, int);
-void process_Input(GLFWwindow*);
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
+void frame_resize_viewport(GLFWwindow *, int, int);
+void process_Input(GLFWwindow *);
 
 float vertices[] = {
     // positions         // colors           // texture coords
-     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   0.0f, 0.0f,   // bottom left
-     0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f,   // top right
-    -0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
+    0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, // bottom left
+    0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,   // top right
+    -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
 };
 float verticesB[] = {
     // positions         // colors           // texture coords
-     0.5f, -0.5f, 0.2f,  1.0f, 0.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-    -0.5f, -0.5f, 0.2f,  0.0f, 1.0f, 0.0f,   0.0f, 0.0f,   // bottom left
-     0.0f,  0.5f, 0.2f,  0.0f, 0.0f, 1.0f,   0.5f, 1.0f    // top 
+    0.5f, -0.5f, 0.2f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.2f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, // bottom left
+    0.0f, 0.5f, 0.2f, 0.0f, 0.0f, 1.0f, 0.5f, 1.0f    // top
 };
 
 float textureCoordinate[] = {
@@ -27,19 +30,21 @@ float textureCoordinate[] = {
 
 };
 
-unsigned int indices[] = {0, 1, 2,  1, 3, 2};
-unsigned int indicesB[] = { 0, 1, 2 };
+unsigned int indices[] = {0, 1, 2, 1, 3, 2};
+unsigned int indicesB[] = {0, 1, 2};
 
 int main()
 {
     // Initialize image loading
     int width, height, nrChannels;
-    unsigned char* imgData = stbi_load("Assets/wall.jpg", &width, &height, &nrChannels, 0);
+    unsigned char *imgData = stbi_load("Assets/wall.jpg", &width, &height, &nrChannels, 0);
 
-    if (!imgData) {
+    if (!imgData)
+    {
         std::cout << "FAILED TO LOAD IMAGE" << std::endl;
     }
-    else {
+    else
+    {
         std::cout << "TEXTURE LOADED CORRECTLY" << std::endl;
     }
 
@@ -48,7 +53,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    GLFWwindow* glfwWindow = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
+    GLFWwindow *glfwWindow = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
 
     if (glfwWindow == NULL)
     {
@@ -57,7 +62,8 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(glfwWindow);
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
         std::cout << "Failed to initialize GLAD" << std::endl;
         glfwTerminate();
         return -1;
@@ -74,97 +80,89 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    if (imgData) {
+    if (imgData)
+    {
         GLenum format = GL_RGB;
-        if (nrChannels == 1) format = GL_RED;
-        else if (nrChannels == 3) format = GL_RGB;
-        else if (nrChannels == 4) format = GL_RGBA;
+        if (nrChannels == 1)
+            format = GL_RED;
+        else if (nrChannels == 3)
+            format = GL_RGB;
+        else if (nrChannels == 4)
+            format = GL_RGBA;
 
         glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, imgData);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     stbi_image_free(imgData);
 
-    unsigned int VAO, VAO2;
-    unsigned int VBO, VBO2;
-    unsigned int EBO, EBO2;
+    VAO vao1;
+    vao1.Bind();
+    VBO vbo1(vertices, sizeof(vertices));
+    EBO ebo1(indices, sizeof(indices));
 
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
+    vao1.LinkAttrib(vbo1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void *)0);
+    vao1.LinkAttrib(vbo1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    vao1.LinkAttrib(vbo1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void *)(6 * sizeof(float)));
 
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    vao1.Unbind();
+    vbo1.Unbind();
+    ebo1.Unbind();
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
+    VAO vao2;
+    vao2.Bind();
+    VBO vbo2(verticesB, sizeof(verticesB));
+    EBO ebo2(indicesB, sizeof(indicesB));
 
-    glGenVertexArrays(1, &VAO2);
-    glGenBuffers(1, &VBO2);
-    glGenBuffers(1, &EBO2);
+    vao2.LinkAttrib(vbo2, 0, 3, GL_FLOAT, 8 * sizeof(float), (void *)0);
+    vao2.LinkAttrib(vbo2, 1, 3, GL_FLOAT, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    vao2.LinkAttrib(vbo2, 2, 2, GL_FLOAT, 8 * sizeof(float), (void *)(6 * sizeof(float)));
 
-    glBindVertexArray(VAO2);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesB), verticesB, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesB), indicesB, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
+    vao2.Unbind();
+    vbo2.Unbind();
+    ebo2.Unbind();
 
     Shader staticShader("shaders/default.vert", "shaders/default.frag");
     Shader animatedShader("shaders/animated.vert", "shaders/animated.frag");
 
-    while (!glfwWindowShouldClose(glfwWindow)) {
+    while (!glfwWindowShouldClose(glfwWindow))
+    {
         float timeValue = (float)glfwGetTime();
 
         process_Input(glfwWindow);
         glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        
 
         staticShader.use();
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        glBindVertexArray(VAO);
+        vao1.Bind();
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        
+
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        glBindVertexArray(VAO);
+        vao1.Bind();
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        
+
         glBindTexture(GL_TEXTURE_2D, texture);
 
         animatedShader.use();
         animatedShader.setFloat("timeValue", timeValue);
 
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        glBindVertexArray(VAO2);
+        vao2.Bind();
         glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-        
+
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        glBindVertexArray(VAO2);
+        vao2.Bind();
         glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(glfwWindow);
         glfwPollEvents();
     }
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
-    glDeleteVertexArrays(1, &VAO2);
-    glDeleteBuffers(1, &VBO2);
-    glDeleteBuffers(1, &EBO2);
+    vao1.Delete();
+    vbo1.Delete();
+    ebo1.Delete();
+    vao2.Delete();
+    vbo2.Delete();
+    ebo2.Delete();
     glDeleteTextures(1, &texture);
 
     std::cout << "Terminating Program" << std::endl;
@@ -173,12 +171,15 @@ int main()
     return 0;
 }
 
-void frame_resize_viewport(GLFWwindow* window, int width, int height) {
+void frame_resize_viewport(GLFWwindow *window, int width, int height)
+{
     glViewport(0, 0, width, height);
 }
 
-void process_Input(GLFWwindow* window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+void process_Input(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
         glfwSetWindowShouldClose(window, true);
     }
 }
